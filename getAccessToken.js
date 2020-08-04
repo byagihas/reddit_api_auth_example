49bb61e1fcf1d2a534aeb3f37b3bcb7c5ca4608a
BY <byagihas@gmail.com>
2020-08-03 21:21:16 -0500

    init

---
+++ getAccessToken.js
@@ -0,0 +1,71 @@
+'use strict';
+
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const request_promise = require('request-promise');
+const querystring = require('querystring');
+
+// Set up authentication from .env using dotenv package.
+// .env file is ignored in this repo, you must set it up locally with params below:
+// REDDIT_API_SECRET=
+// REDDIT_API_CLIENT=
+// REDDIT_REDIRECT_URI=
+// REDDIT_USER=
+// REDDIT_PASS=
+const CLIENT_ID = process.env.REDDIT_API_CLIENT;
+const CLIENT_SECRET = process.env.REDDIT_API_SECRET;
+const USER = process.env.REDDIT_USER;
+const PASS = process.env.REDDIT_PASS;
+
+// Function: getToken
+// Retrieves token using client_id, client_secret, usr(username), and pswd(password)
+// using Request package to make a POST call to the Reddit API
+const getToken = (cliend_id, client_secret, usr, pswd) => {
+    const accessuri = `https://www.reddit.com/api/v1/access_token`;
+    const data_object = {grant_type: "password", username: usr, password: pswd};
+    const post_data = querystring.stringify(data_object);
+    const length = post_data.length;
+
+    // Headers with Basic Auth
+    let req_headers = {
+        'Authorization' : 'Basic ' + new Buffer.from(cliend_id + ':' + client_secret).toString('base64'),
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Content-Length' : length
+    };
+
+    // Promisify request for now, might use request-promise instead
+    // more logical syntax with resolve and reject to return the token
+    return new Promise((resolves, rejects) => {
+        request({uri: accessuri, headers: req_headers, body : post_data, method: 'POST'}, (error, response) => {
+            let access_object = JSON.parse(response.body);
+            resolves(access_object);
+        }).on('error', (error) => {
+            let Error = new Error(error);
+            rejects(Error);
+        });
+    });
+};
+
+// Run getToken  and Store token in token.json for modular use
+const main = () => {
+    getToken(CLIENT_ID, CLIENT_SECRET, USER, PASS)
+        .then(object => {
+            fs.writeFileSync(__dirname + '/token.json', JSON.stringify(object));
+            console.log("Token written to " + __dirname + '\\token.json');
+            return object;
+        }).catch(error => {
+            throw new Error(error);
+        });
+};
+
+main();
+
+exports.getToken = main;
+
+
+
+
---
+++ getPostsFromSubreddit.js
@@ -0,0 +1,41 @@
+'use strict';
+
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const request_promise = require('request-promise');
+const querystring = require('querystring');
+const accessToken = require('./token.json');
+
+const CLIENT_ID = process.env.REDDIT_API_CLIENT;
+const CLIENT_SECRET = process.env.REDDIT_API_SECRET;
+const USER = process.env.REDDIT_USER;
+const PASS = process.env.REDDIT_PASS;
+
+let token = accessToken.access_token;
+console.log("Token retrieved: " + token);
+
+// Function: getRecipes()
+// Example to retrieve posts about food recipes and store using Access Token Auth for Reddit API
+// parameters: subreddit - subreddit to search for recipes
+const getPostsFromSubreddit = (subreddit) => {
+    const count = '10';
+    const sorttype = 'new';
+    const timeinterval = 'hour';
+    let req_headers = {
+        'Authorization' : 'bearer ' + token,
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
+    };
+    let posts = {};
+    request_promise({uri: `https://reddit.com/r/${subreddit}/.json?count=${count}?sort=${sorttype}?t=${timeinterval}`, headers: req_headers, method: 'GET'}, (error, response) => {
+        let body_parsed = JSON.parse(response.body);
+        posts = JSON.stringify(body_parsed.data.children);
+    }).then((posts) => {
+        fs.writeFileSync(__dirname + '/data/possibleRecipePosts.json', posts);
+        console.log('File written to: ' + __dirname + '/data/possibleRecipePosts.json');
+    });
+};
+
+// getting posts from r/food
+getPostsFromSubreddit('food');
